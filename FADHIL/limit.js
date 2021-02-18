70514f625585f9ac6eaf008690620aebc8b9fca3
SrAllison52 <78006833+allison5200a@users.noreply.github.com>
2021-02-18 02:10:30 -0300

    Update limit.js

--- FADHIL/limit.js
+++ FADHIL/limit.js
@@ -1,13 +1,13 @@
 const limitend = (pushname2) => {
-        return`*maaf ${pushname2} limit hari ini habis*\n*limit di reset setiap jam 12:00 WIB TENGAH MALAM*`
+        return`*Desculpe ${pushname2}o limite de hoje expirou*\n*o limite é redefinido a cada 12:00 WIB MIDDLE NIGHT*`
 }
 
 const limitcount = (limitCounts) => {
         return`
 *「 LIMIT COUNT 」*
-sisa limit anda : ${limitCounts}
+o resto do seu limite : ${limitCounts}
 
-NOTE : Kalo Habis Kalian Bisa Gunakan Lagi Besok`
+NOTE : Se terminar, você pode usá-lo novamente amanhã`
 }
 exports.limitend = limitend
 exports.limitcount = limitcount
