d001b6687d7f2900d9214cbecee302b046935ac7
SrAllison52 <78006833+allison5200a@users.noreply.github.com>
2021-02-18 02:16:40 -0300

    Update makermenu.js

--- FADHIL/makermenu.js
+++ FADHIL/makermenu.js
@@ -1,39 +1,39 @@
 const makermenu = (prefix) => { 
 	return ` 
-╭───「 *CREATOR MENU* 」───
+╭───「 *MENU CRIADOR* 」───
 │
 ├➲ *${prefix}sticker*
-├➲ *${prefix}quotemaker [tx/wtrmk/tema]*
+├➲ *${prefix}quotemaker [txt/wtrmk/tema]*
 ├➲ *${prefix}nulis [nama/kelas/text]*
-├➲ *${prefix}trigger [reply image]*
-├➲ *${prefix}rip [reply image]*
-├➲ *${prefix}wasted [reply image]*
-├➲ *${prefix}cphlogo [Fadhil/Graphy]*
-├➲ *${prefix}cglitch [Fadhil/Graphy]*
-├➲ *${prefix}cpubg [Fadhil/Graphy]*
-├➲ *${prefix}cml [Salsa/Fadhil]*
-├➲ *${prefix}tahta [Fadhil]*
-├➲ *${prefix}croman [Fadhil dan Salsa]*
-├➲ *${prefix}cthunder [Fadhil]*
-├➲ *${prefix}cbpink [Fadhil]*
-├➲ *${prefix}cmwolf [Fadhil]*
-├➲ *${prefix}csky [Fadhil]*
-├➲ *${prefix}cwooden [Fadhil]*
-├➲ *${prefix}cflower [Fadhil]*
-├➲ *${prefix}clove [Fadhil]*
-├➲ *${prefix}ccrossfire [Fadhil]*
-├➲ *${prefix}cnaruto [Fadhil]*
-├➲ *${prefix}cparty [Fadhil]*
-├➲ *${prefix}cshadow [Fadhil]*
-├➲ *${prefix}cminion [Fadhil]*
-├➲ *${prefix}cneon [Fadhil]*
-├➲ *${prefix}cneon2 [Fadhil]*
-├➲ *${prefix}cneongreen [Fadhil]*
-├➲ *${prefix}c3d [Fadhil]*
-├➲ *${prefix}csky [Fadhil]*
-├➲ *${prefix}tts [id Fadhil]*
-├➲ *${prefix}ttp [Fadhil]*
-├➲ *${prefix}slide [Fadhil]*
+├➲ *${prefix}trigger [imagem de resposta]*
+├➲ *${prefix}rip [imagem de resposta]*
+├➲ *${prefix}wasted [imagem de resposta]*
+├➲ *${prefix}cphlogo [texto1/texto2]*
+├➲ *${prefix}cglitch [texto1/texto2]*
+├➲ *${prefix}cpubg [texto1/texto2]*
+├➲ *${prefix}cml [texto1/texto2]*
+├➲ *${prefix}tahta [seu texto]*
+├➲ *${prefix}croman [ALN dan Salsa]*
+├➲ *${prefix}cthunder [seu texto]*
+├➲ *${prefix}cbpink [seu texto]*
+├➲ *${prefix}cmwolf [seu texto]*
+├➲ *${prefix}csky [seu texto]*
+├➲ *${prefix}cwooden [seu texto]*
+├➲ *${prefix}cflower seu texto]*
+├➲ *${prefix}clove [seu texto]*
+├➲ *${prefix}ccrossfire [seu texto]*
+├➲ *${prefix}cnaruto [seu texto]*
+├➲ *${prefix}cparty [seu texto]*
+├➲ *${prefix}cshadow [seu texto]*
+├➲ *${prefix}cminion [seu texto]*
+├➲ *${prefix}cneon [seu texto]*
+├➲ *${prefix}cneon2 [seu texto]*
+├➲ *${prefix}cneongreen [seu texto]*
+├➲ *${prefix}c3d [seu texto]*
+├➲ *${prefix}csky [seu texto]*
+├➲ *${prefix}tts pt [seu texto]*
+├➲ *${prefix}ttp [seu texto]*
+├➲ *${prefix}slide [seu texto]*
 ├➲ *${prefix}stiker*
 ├➲ *${prefix}gifstiker*
 ├➲ *${prefix}toimg*
@@ -44,6 +44,6 @@ const makermenu = (prefix) => {
 │
 ╰──────────────────────
 
-	           *©Fadhil Graphy*`
+	           *©ᴀʟɴ ʙᴏᴛs*`
 	}
 exports.makermenu = makermenu
